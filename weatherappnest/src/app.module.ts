092db507f3bae0e03bfe5183cffcddb41a75bdb3
yinotherhino <muhammedbayero@gmail.com>
2023-03-03 00:25:35 +0100

    middleware

--- weatherappnest/src/app.module.ts
+++ weatherappnest/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ForecastsModule } from './forecasts/forecasts.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { DatabaseModule } from './database/database.module';
 import Config from './config';
+import { AuthMiddleware } from './middleware/apikey.middleware';
 
 @Module({
   imports: [ForecastsModule, SequelizeModule.forRoot(Config.db), DatabaseModule,
@@ -13,4 +14,10 @@ import Config from './config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('api/v1/');
+  }
+ }
