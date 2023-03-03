146482525daa00eb69456a1e6368738f102fad30
yinotherhino <muhammedbayero@gmail.com>
2023-03-03 10:47:29 +0100

    env prod

--- weatherapp/src/environments/environment.prod.ts
+++ weatherapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://seams-weathered.netlify.app/api/v1/forecasts',
+  apiUrl: 'https://weather-api-nest.onrender.com/api/v1/forecasts',
   apiKey: 'API_KEY'
 };
